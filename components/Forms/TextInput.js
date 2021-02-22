292e2368d3b109a1ea0cdf1fcef952eee715ab43
Kristian Espina <kespina@iripple.com>
2021-02-22 18:54:36 +0800

    Updates on design

--- components/Forms/TextInput.js
+++ components/Forms/TextInput.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
-const TextInput = ( {caption} ) => {
+const TextInput = ( {caption, state, setState} ) => {
+    const handleChange = (e) => {
+        
+    }
     return (
+        <>
         <div class="my-5 text-sm">
-            <label for="username" class="block text-gray-700 font-semibold">{caption}</label>
-            <input type="text" autoFocus id="username" class="rounded-md px-4 py-2 mt-3 focus:outline-none bg-gray-100 w-full border-none" placeholder="" />
+            <label for="username" class="block uppercase text-sm text-gray-500 font-semibold">{caption}</label>
+            <input type="text" autoFocus id="username" class="rounded-md px-4 py-2 mt-2 focus:outline-none bg-gray-100 w-full border-none" placeholder="" />
         </div>
+        <div class="flex flex-wrap items-stretch w-full mb-4 relative">
+            <label class="block uppercase text-sm text-gray-500 font-semibold">{caption}</label>
+            <input type="text" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 h-10 rounded rounded-r-none px-3 relative bg-gray-100 border-none" placeholder="" />
+            <div class="flex -mr-px">
+                <span class="flex items-center leading-normal bg-grey-lighter rounded rounded-l-none border border-l-0 border-grey-light px-3 whitespace-no-wrap text-grey-dark text-sm">
+                    kWp
+                </span>
+            </div>	
+        </div>
+        </>
     )
 }
 
--- components/Main.js
+++ components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import TextInput from "./Forms/TextInput"
 import Statistics from "./Statistics"
@@ -11,33 +11,52 @@ import ChartInfographic from "./Icons/ChartInfographic"
 import Coin from "./Icons/Coin"
 
 const Main = () => {
+    const [systemPower, setSystemPower] = useState()
+    const [contractPrice, setContractPrice] = useState()
+    const [peakSunHours, setPeakSunHours] = useState()
+
+    const [electricBill, setElectricBill] = useState()
+    const [buyingPrice, setBuyingPrice] = useState()
+    const [sellingPrice, setSellingPrice] = useState()
+
     return (
         <div className="flex flex-col bg-gray-50 h-full w-full">
             <div className="bg-gradient-to-r from-blue-800 to-blue-500 w-full h-72 z-0">
             </div>
 
-            <div className="bg-white -mt-36 mb-12 z-50 ml-10 mr-10 p-5 rounded-md">
+            <div className="bg-white -mt-36 mb-12 z-50 ml-10 mr-10 p-5 rounded-md shadow-sm">
                 <p className="text-2xl font-semibold mb-5">Savings Simulator</p>
                 <div className="flex sm:flex-col md:flex-row w-100">
 
-                    <div className="flex-1 pr-5">
-                        <p className="text-lg font-semibold text-gray-700">Solar Panel Info</p>
-                        <TextInput caption="System Power" />
-                        <TextInput caption="Contract Price" />
-                        <TextInput caption="Peak Sun Hours" />
-
-                        <p className="text-lg font-semibold text-gray-700">Electric Bill</p>
-                        <TextInput caption="Monthly Electric Bill" />
-                        <TextInput caption="Buying Price" />
-                        <TextInput caption="Selling Price" />
+                    <div className="flex-1">
+                        <div className="flex flex-col md:flex-row">
+                            <div className="flex-1 md:pr-5">
+                                <p className="text-lg font-semibold text-gray-700">Solar Panel Info</p>
+                                <TextInput caption="System Power" state={systemPower} setState={setSystemPower} />
+                                <TextInput caption="Contract Price" state={contractPrice} setState={setContractPrice} />
+                                <TextInput caption="Peak Sun Hours" state={peakSunHours} setState={setPeakSunHours} />
+                            </div>
+                            <div className="flex-1 md:pr-5">
+                                <p className="text-lg font-semibold text-gray-700">Electric Bill</p>
+                                <TextInput caption="Monthly Electric Bill" state={electricBill} setState={setElectricBill} />
+                                <TextInput caption="Buying Price" state={buyingPrice} setState={setBuyingPrice} />
+                                <TextInput caption="Selling Price" state={sellingPrice} setState={setSellingPrice} />
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex-1 pl-5">
-                        <Statistics icon={<Sun />} color="yellow" caption="Power Produced (kWh/d)" value="22.5"/>
-                        <Statistics icon={<Charging />} color="yellow" caption="Power Used (kWh/d)" value="22.5"/>
-                        <Statistics icon={<Bolt />} color="yellow" caption="Power Exported (kWh/d)" value="22.5"/>
-                        <Statistics icon={<Coin />} color="green" caption="Savings (%)" value="22.5"/>
-                        <Statistics icon={<ChartInfographic />} color="green" caption="ROI (w/o Net Metering)" value="22.5"/>
-                        <Statistics icon={<ChartInfographic />} color="green" caption="ROI (w/ Net Metering)" value="22.5"/>
+                    <div className="flex-1">
+                        <div className="flex flex-col md:flex-row">
+                            <div className="flex-1 md:pr-5">
+                                <Statistics icon={<Sun />} color="yellow" caption="Power Produced (kWh/d)" value="22.5"/>
+                                <Statistics icon={<Charging />} color="yellow" caption="Power Used (kWh/d)" value="22.5"/>
+                                <Statistics icon={<Bolt />} color="yellow" caption="Power Exported (kWh/d)" value="22.5"/>
+                            </div>
+                            <div className="flex-1">
+                                <Statistics icon={<Coin />} color="green" caption="Savings (%)" value="22.5"/>
+                                <Statistics icon={<ChartInfographic />} color="green" caption="ROI (w/o Net Metering)" value="22.5"/>
+                                <Statistics icon={<ChartInfographic />} color="green" caption="ROI (w/ Net Metering)" value="22.5"/>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
